507d838f511d2d14b2f2981a5a074721df780790
Joseph <jebranzuela@gmail.com>
2018-08-30 09:36:04 +0800

    Final result for link checker

--- getbrowser.py
+++ getbrowser.py
@@ -3,5 +3,5 @@ import webbrowser
 with open('links_with_error_2.txt', 'r') as f:
     links = [line.strip() for line in f]
 
-for url in urls:
-	webbrowser.open(url)
+for link in links:
+	webbrowser.open(link)
